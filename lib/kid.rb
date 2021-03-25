3fadfcf10fd0ebddee48b66804d36f2ea3665e8d
Daniel Abraham Braude <daniboy1007@gmail.com>
2021-03-25 18:02:45 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,7 +2,7 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 class Kid
   extend FancyDance::ClassMethods
- include FancyDance::InstanceMethods
+  include FancyDance::InstanceMethods
 
 
  attr_accessor :name
@@ -10,4 +10,4 @@ class Kid
  def initialize(name)
    @name = name
  end
-end 
+end
