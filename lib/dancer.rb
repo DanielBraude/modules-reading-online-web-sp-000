848e4d59190af2e862baef43791dda5bf406ffbd
Daniel Abraham Braude <daniboy1007@gmail.com>
2021-03-25 18:04:02 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,12 +1,17 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './dance_module.rb'
+require_relative "fancy_dance.rb"
+
 class Dancer
   extend FancyDance::ClassMethods
-   include FancyDance::InstanceMethods
+  include FancyDance::InstanceMethods
 
-   attr_accessor :name
+  attr_accessor :name
 
-   def initialize(name)
-     @name = name
-   end
- end 
+  def initialize(name)
+    @name = name
+  end
+
+
+end 
